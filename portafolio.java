f883585ddbeb110d70d09924ac23795047dd4441
2145578 <jbdescobar@scl.edu.gt>
2025-03-18 08:58:50 -0600

    Add files via upload

---
+++ portafolio.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class portafolio {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opciones ;
+        
+
+            System.out.println("\n \n");
+            System.out.println("Bienvenidos a mi escobar java [escobar versión 1.0]");
+
+            System.out.println("_____ ____   ____ ___  ____    _    ____");
+            System.out.println("| ____/ ___| / ___/ _ \\| __ )  / \\  |  _ \\");
+            System.out.println("|  _| \\___ \\| |  | | | |  _ \\ / _ \\ | |_) |");
+            System.out.println("| |___ ___) | |__| |_| | |_) / ___ \\|  _ <");
+            System.out.println("|_____|____/ \\____\\___/|____/_/   \\_\\_| \\_\\");
+
+
+
+
+
+            System.out.println("+---------------------+");
+            System.out.println("[1]. Suma de dos números");
+            System.out.println("[2]. Opcion-1");
+            System.out.println("[3]. Opcion-2");
+            System.out.println("[4]. Opcion-3");
+            System.out.println("[5]. Salir");
+            System.out.print("elija una opcion: ");
+          
+
+        
+    }
+}
